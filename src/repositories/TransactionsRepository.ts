1220da57fabca1f716e79f61ca889559e9c2e205
Felipe Silva <felipe.dacs@gmail.com>
2020-04-16 02:21:51 -0300

    ok

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,33 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((prev, cur) => {
+      if (cur.type === 'income') {
+        return prev + cur.value;
+      }
+      return prev + 0;
+    }, 0);
+    const outcome = this.transactions.reduce((prev, cur) => {
+      if (cur.type === 'outcome') {
+        return prev + cur.value;
+      }
+      return prev + 0;
+    }, 0);
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
